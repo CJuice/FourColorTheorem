c1db3de4b87ad538dc2f87fb3e0d98eeeea4c3f3
CJuice <CJuice@users.noreply.github.com>
2017-09-06 11:28:27 -0500

    supporting class

---
+++ AdjacentSelectionClass.py
@@ -0,0 +1,33 @@
+###################################
+# Script:  AdjacentSelectionClass.py
+# Author:  Conrad Schaefer
+# Date Created:  09/06/2017
+# Purpose:  This class gets imported into the main script. It defines a method that returns a layer with adjacent polygons selected.
+# Inputs:  Workspace, Scratch Workspace, Feature Layer of Interest
+# Outputs:  None but messages to the geoprocessing window and an edited feature class.
+# Modifications: Amended the previous version to use in_memory storage for the tempFC rather than writing it to a
+#                   geodatabase. Creating a new feature class is time consumptive.
+###################################
+class AdjacentSelectionClass(object):
+    '''This class contains a function to be reused by the main script'''
+    def __init__(self, masterFeatureLayer, duplicateFeatureLayer, fieldName):
+        self.masterFeatureLayer = masterFeatureLayer
+        self.duplicateFeatureLayer = duplicateFeatureLayer
+        self.fieldName = fieldName
+
+    def selectAdjacent(self):
+        import arcpy.management
+        #For each feature, select all adjacent features in duplicate that share a line segment
+        selection = arcpy.SelectLayerByLocation_management(in_layer=self.duplicateFeatureLayer,
+                                               overlap_type="SHARE_A_LINE_SEGMENT_WITH",
+                                               select_features=self.masterFeatureLayer,search_distance=None,
+                                               selection_type="NEW_SELECTION",
+                                               invert_spatial_relationship=None)
+        #Remove the feature identical to the selecting feature
+        selection = arcpy.SelectLayerByLocation_management(in_layer=selection,
+                                               overlap_type="ARE_IDENTICAL_TO",
+                                               select_features=self.masterFeatureLayer, search_distance=None,
+                                               selection_type="REMOVE_FROM_SELECTION",
+                                               invert_spatial_relationship=None)
+
+        return selection
